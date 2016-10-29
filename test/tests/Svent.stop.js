243c3e127514ede20e757949c1165c537f4813bd
molingyu <z1422716486@hotmail.com>
2016-10-30 02:31:05 +0800

    init lib

---
+++ test/tests/Svent.stop.js
@@ -0,0 +1,25 @@
+var Svent = require('../../lib/svent.js');
+
+em = new Svent.EventManger();
+
+Svent.run(em, function (em) {
+  em.on('main', {}, function (em) {
+    console.log('Svent#stop start!');
+    em.trigger('stop', "Stop Svent!");
+    console.log('stop!');
+  });
+
+  em.on('stop', {}, (_, data)=>{
+    Svent.stop();
+    console.log(data)
+  });
+
+  em.trigger('main')
+});
+
+// out put
+/*
+ Svent#stop start!
+ stop!
+ Stop Svent!
+ */
